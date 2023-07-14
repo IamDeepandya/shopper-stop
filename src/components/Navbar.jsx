fccde1e373010e381ee3e4cf8736a40a765f2df4
Murtaza Motagam <108859240+Murtaza-Motagam@users.noreply.github.com>
2023-07-14 18:10:34 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 const Navbar = (props) => {
 
+ const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  
   const location = useLocation();
 
   const items = useSelector((state) => state.cart)
@@ -12,31 +17,117 @@ const Navbar = (props) => {
 
   return (
     <>
-      <header className={`body-font navbar-${props.mode} lg:sticky lg:top-0 lg:z-50`}>
-        <div className={`container-fluid bs-1 navbar-${props.mode} mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center`}>
-          <Link to="/" className="flex title-font font-medium items-center mb-4 md:mb-0">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span className="ml-3 text-xl  font-fam-poppins font-bold">Shopper-Stop</span>
-          </Link>
-          <nav className="  md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center font-fam-roboto">
-            <Link to="/products" className={`mr-5 hover:text-gray-300 ${location.pathname === "/products" ? "active" : ""}`}>Products</Link>
-            <Link to="/services" className={`mr-5 hover:text-gray-300 ${location.pathname === "/services" ? "active" : ""}`}>Services</Link>
-            <Link to="/about" className={`mr-5 hover:text-gray-300 ${location.pathname === "/about" ? "active" : ""}`}>About</Link>
-            <Link to="/contact" className={`mr-5 hover:text-gray-300 ${location.pathname === "/contact" ? "active" : ""}`}>Contact</Link>
-          </nav>
-          <div className="">
-            <button className={`mr-3 font-fam-roboto inline-flex items-center modal-mode-${props.mode} border-0 py-1 px-3 focus:outline-none  rounded text-base mt-4 md:mt-0`}>
-              <i className=" mr-2 fa-solid fa-right-to-bracket fa-sm" style={{ color: "#4b0ff0" }}></i>
-              Register
-            </button>
-            <Link to="/cart" className='mr-5 font-fam-roboto bg-transparent font-semibold px-4 py-2 border py-1 rounded'><i className=" mt-4 mr-2 fa-solid fa-cart-shopping fa-lg"></i>{items.length}</Link>
-            <i id="moon" className="icon-hover fa-solid fa-moon fa-2xl cursor-pointer " onClick={props.toggleMode}></i>
-
+      <nav className="bg-main font-fam-roboto">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <Link to="/" className="text-white lg:text-2xl md:text-2xl font-bold sm:text-lg">
+                  Shopper-Stop
+                </Link>
+              </div>
+            </div>
+            <div className="hidden md:block ">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <Link
+                  to="/products"
+                  className={`text-white px-3 py-2 rounded-md text-[30px] font-medium active-link-hover ${location.pathname === '/products' ? 'active' : ''}`}
+                >
+                  Products
+                </Link>
+                <Link
+                  to="/services"
+                  className={`text-white px-3 py-2 rounded-md text-[30px] font-medium  active-link-hover ${location.pathname === '/services' ? 'active' : ''}`}
+                >
+                  Services
+                </Link>
+                <Link
+                  to="/about"
+                  className={`text-white px-3 py-2 rounded-md text-[30px] font-medium active-link-hover ${location.pathname === '/about' ? 'active' : ''}`}
+                >
+                  About
+                </Link>
+                <Link
+                  to="/contact"
+                  className={`text-white px-3 py-2 rounded-md text-[30px] font-medium active-link-hover ${location.pathname === '/contact' ? 'active' : ''}`}
+                >
+                  Contacts
+                </Link>
+                <Link to="/cart" className='mr-2 font-fam-roboto bg-transparent font-semibold  text-white rounded'><i className=" mr-2 fa-xl fa-solid fa-cart-shopping "></i>{items.length}</Link>
+                <Link className='ml-2'>
+                  <i id="moon" className="icon-hover text-white fa-solid fa-moon fa-2xl cursor-pointer " onClick={props.toggleMode}></i>
+                </Link>
+              </div>
+            </div>
+            <div className="md:hidden">
+              <button
+                type="button"
+                onClick={toggleMenu}
+                className="inline-flex items-center justify-center p-2 rounded-md text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded={isOpen}
+              >
+                <span className="sr-only">Open main menu</span>
+                <svg
+                  className={`h-6 w-6 ${isOpen ? 'hidden' : 'block'}`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+                <svg
+                  className={`h-6 w-6 ${isOpen ? 'block' : 'hidden'}`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
-      </header>
+
+        <div className={`md:hidden ${isOpen ? 'block' : 'hidden'}`} id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <Link
+              to="/products"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Products
+            </Link>
+            <Link
+              to="/services"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Services
+            </Link>
+            <Link
+              to="/contacts"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Contacts
+            </Link>
+            <Link
+              to="/login"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Login
+            </Link>
+            <Link
+              to="/signup"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </nav>
 
 
 
